3e34a11d2f2599160b340ed39b6cf8d402484922
angelo <aurtula1@gmail.com>
2021-06-17 12:52:02 -0400

    commit tree 18d3d3d208fd9474f19a9f86bd6ef52e154477b8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(data => {
+    let h3 = document.createElement("h3")
+    h3.textContent = data.id
+    document.body.appendChild(h3)
+    console.log(data)
+    })
+    .catch((error) => {
+        let h2 = document.createElement("h2")
+        h2.innerHTML = error.message
+        document.body.appendChild(h2)
+    })
+   
+}
